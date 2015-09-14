9f5410a916043c42fab2d991bda8baf19eca69cd
Yogesh <yogeshvaity@gmail.com>
2015-09-14 15:07:39 +0530

    rectify ex3 repo mix up

---
+++ ch13/ex3/GreenhouseController.java
@@ -0,0 +1,29 @@
+//: c08:GreenhouseController.java
+// Configure and execute the greenhouse system.
+// {Args: 5000}
+// From 'Thinking in Java, 3rd ed.' (c) Bruce Eckel 2002
+// www.BruceEckel.com. See copyright notice in CopyRight.txt.
+
+public class GreenhouseController {
+  public static void main(String[] args) {
+    GreenhouseControls gc = new GreenhouseControls();
+    // Instead of hard-wiring, you could parse
+    // configuration information from a text file here:
+    gc.addEvent(gc.new Bell(900));
+    Event[] eventList = {
+      gc.new ThermostatNight(0),
+      gc.new LightOn(200),
+      gc.new LightOff(400),
+      gc.new WaterOn(600),
+      gc.new WaterOff(800),
+      gc.new ThermostatDay(1400),
+	  gc.new FanOn(1500),
+	  gc.new FanOff(1600)
+    };
+    gc.addEvent(gc.new Restart(2000, eventList));
+    if(args.length == 1)
+      gc.addEvent(
+        gc.new Terminate(Integer.parseInt(args[0])));
+    gc.run();
+  }
+} ///:~
