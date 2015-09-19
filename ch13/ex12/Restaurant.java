50be4b7481614922c13a0f8b7c46b29e28d4af61
Yogesh <yogeshvaity@gmail.com>
2015-09-19 16:01:20 +0530

    system working, but not exiting after last WP gets order

--- ch13/ex12/Restaurant.java
+++ ch13/ex12/Restaurant.java
@@ -7,19 +7,23 @@
  */
  
 import java.util.*;
-import com.bruceeckel.simpletest.*;
 
 class Order {
   // Reference that Chef will use to notify
   public WaitPerson wp;
+  private final int MAX_ORDERS = 10;
   private static int i = 0;
   private int count = i++;
   public Order(WaitPerson wp) {
     this.wp = wp;
-    if(count == 10) {
-      System.out.println("Out of food, closing");
-      System.exit(0);
-    }
+//    if(count == 10) {
+//      System.out.println("Out of food, closing");
+//      System.exit(0);
+//    }
+  }
+  public boolean isLastOrder() {
+    if(count >= MAX_ORDERS) return true;
+    else return false;
   }
   // To notify the respective WaitPerson
   public void notifyWP() {
@@ -43,15 +47,21 @@ class WaitPerson extends Thread {
     start();
   }
   private Order order;
+  private boolean lastOrder = false;
   public void run() {
     while(true) {
       // WaitPerson generates an order and adds it to the
       // incomingOrders and goes on wait()
       synchronized(restaurant.outgoingOrders) {
         order = new Order(this);
-        System.out.println(Thread.currentThread().getName() 
-        + " generated " + order);
-        restaurant.incomingOrders.add(order);
+        if(!order.isLastOrder()) {
+          System.out.println(Thread.currentThread().getName() 
+          + " generated " + order);
+          restaurant.incomingOrders.add(order);
+        } else if(order.isLastOrder()) {
+          lastOrder = true;
+        }
+        
       }
       synchronized(this) {
         try {
@@ -111,13 +121,12 @@ class Chef extends Thread {
           order.notifyWP();
         }
       }
-      yield();
+      yield(); // To give other Chefs a chance to prepare order.
     }
   }
 }
 
 public class Restaurant {
-  private static Test monitor = new Test();
   
   Queue incomingOrders = new LinkedList(); // incoming for chef
   Queue outgoingOrders = new LinkedList(); // outgoing for chef
@@ -131,9 +140,18 @@ public class Restaurant {
     new WaitPerson("WP Gotu", restaurant);
     WaitPerson waitPerson3 = 
     new WaitPerson("WP Motu", restaurant);
+    WaitPerson waitPerson4 = 
+    new WaitPerson("WP Pinku", restaurant);
+    WaitPerson waitPerson5 = 
+    new WaitPerson("WP Lambu", restaurant);
+    WaitPerson waitPerson6 = 
+    new WaitPerson("WP Dhondu", restaurant);
     
     Chef chef = new Chef("Chef Singhania", restaurant);
     Chef chef2 = new Chef("Chef Malya", restaurant);
     Chef chef3 = new Chef("Chef Ambani", restaurant);
+    Chef chef4 = new Chef("Chef Birla", restaurant);
+    Chef chef5 = new Chef("Chef Tata", restaurant);
+    Chef chef6 = new Chef("Chef KrishnaMurti", restaurant);
   }
 } ///:~
