66ddba7a47df73e5dbf9c8c05c5172aacdb7ad9a
Yogesh <yogeshvaity@gmail.com>
2015-09-20 18:33:19 +0530

    finish 13ex14

--- ch13/ex14/PipedIO.java
+++ ch13/ex14/PipedIO.java
@@ -6,18 +6,28 @@ import java.io.*;
 import java.util.*;
 
 class Sender extends Thread {
+  private BufferedReader readFile;
+  public Sender(BufferedReader readFile) {
+    this.readFile = readFile;
+  }
   private Random rand = new Random();
   private PipedWriter out = new PipedWriter();
   public PipedWriter getPipedWriter() { return out; }
+  
+  private String s;
+  @Override
   public void run() {
     while(true) {
-      for(char c = 'A'; c <= 'z'; c++) {
-        try {
-          out.write(c);
-          sleep(rand.nextInt(500));
-        } catch(Exception e) {
-          throw new RuntimeException(e);
+      try {
+        while((s = readFile.readLine()) != null) {
+          out.write(s);
+          out.write("\n");
+          sleep(rand.nextInt(100));
         }
+      } catch(IOException e) {
+        throw new RuntimeException(e);
+      } catch(InterruptedException ie) {
+        System.out.println("Sender's sleep interrupted");
       }
     }
   }
@@ -32,7 +42,7 @@ class Receiver extends Thread {
     try {
       while(true) {
         // Blocks until characters are there:
-        System.out.println("Read: " + (char)in.read());
+        System.out.print((char)in.read());
       }
     } catch(IOException e) {
       throw new RuntimeException(e);
@@ -42,10 +52,13 @@ class Receiver extends Thread {
 
 public class PipedIO {
   public static void main(String[] args) throws Exception {
-    Sender sender = new Sender();
+    // this part must be in the executable block
+    // not as field in a class
+    BufferedReader readFile = new BufferedReader(new FileReader(new File("PipedIO.java")));
+    Sender sender = new Sender(readFile);
     Receiver receiver = new Receiver(sender);
     sender.start();
     receiver.start();
-    new Timeout(4000, "Terminated");
+    new Timeout(6000, "Terminated");
   }
 } ///:~
