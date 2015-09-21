3a15fb2bd266ec22c243c06eb3310a0898c43436
Yogesh <yogeshvaity@gmail.com>
2015-09-21 20:39:35 +0530

    deadlocking with - java DiningPhilosophers 6 0

--- ch13/ex15/DiningPhilosophers.java
+++ ch13/ex15/DiningPhilosophers.java
@@ -6,7 +6,18 @@
  * chopsticks from the bin). Does this eliminate the possibility 
  * of deadlock? Can you re-introduce deadlock by simply reducing 
  * the number of available chopsticks?
+ 
+ 
+ * Conclusion : 
+ 
+ * 1. java DiningPhilosophers 6 0
+ * If philosophers are allowed to take 1 chopstick at a time, 
+ * the program, deadlocks after a little while
+ 
+ * 2. 
  */
+ 
+ 
  
 import java.util.*;
 
@@ -22,10 +33,11 @@ class Philosopher extends Thread {
   private static Random rand = new Random();
   private static int counter = 0;
   private int number = counter++;
-  private Chopstick[] allChopSticks;
+  private LinkedList allChopSticks;
   static int ponder = 0; // Package access
   // has references to all chopsticks
-  public Philosopher(List allChopSticks) {
+  public Philosopher(String name, LinkedList allChopSticks) {
+    super(name);
     this.allChopSticks = allChopSticks;
     start();
   }
@@ -45,15 +57,14 @@ class Philosopher extends Thread {
     // and return the eat method so that he won't try to get lock
     // on all chopsticks.
     // the return point should be after synchronized block
-    List chopstickList = new Arraylist(); // stores 2 chopsticks
+    List chopstickList = new ArrayList(); // stores 2 chopsticks
     // Get chopsticks from bin
     System.out.println(this + 
     " needs to eat, waiting for chopsticks...");
-    //int availableChopsticks = chopstickList.size();
     while(chopstickList.size() < 2) {
       synchronized(allChopSticks) {
         if(!allChopSticks.isEmpty()) {
-          chopstickList.add(allChopSticks.remove(1));
+          chopstickList.add(allChopSticks.removeFirst());
           if(chopstickList.size() == 1) {
             System.out.println(this + " has a chopstick" + 
             "waiting for another...");
@@ -103,12 +114,15 @@ public class DiningPhilosophers {
     Philosopher[] philosopher =
       new Philosopher[Integer.parseInt(args[0])];
     Philosopher.ponder = Integer.parseInt(args[1]);
-    List allChopSticks = new Arraylist();
+    LinkedList allChopSticks = new LinkedList();
     for(int i = 0; i < 5; i++) {
       allChopSticks.add(new Chopstick());
     }
     for(int i = 0; i < philosopher.length; i++) {
-      philosopher[i] = new Philosopher(allChopSticks);
+      // give each thread a name so that it becomes easier to
+      // recognize while solving exceptions
+      philosopher[i] = new Philosopher("Philosopher " + 
+      i, allChopSticks);
     }
   }
 } ///:~
