edf6a0642848dc0f2ac1fec1fd32c9ca82d39e25
Yogesh <yogeshvaity@gmail.com>
2015-10-12 21:10:10 +0530

    finish 14ex3

--- ch14/ex3/JPasswordField.java
+++ ch14/ex3/JPasswordField.java
@@ -9,12 +9,36 @@
  import javax.swing.*;
  import java.awt.*;
  import java.awt.event.*;
- import com.bruceeckel.swing.Console;
+ import com.bruceeckel.swing.*;
+ import java.util.*;
  
  public class JPasswordField extends JApplet {
-  private JPasswordField passField = new JPasswordField();
+  private JTextField tf = new JTextField(12);
+  private javax.swing.JPasswordField passField = new javax.swing.JPasswordField(12);
   @Override
   public void init() {
+    passField.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        boolean isCorrect = true;
+        char[] input = passField.getPassword();
+        char[] correctPassword = 
+        { 'y', 'o', 'u', 't', 'u', 'b', 'e'};
+        if(input.length != correctPassword.length) {
+          isCorrect = false;
+        } else {
+          isCorrect = Arrays.equals(input, correctPassword);
+        }
+        if (isCorrect) {
+          JOptionPane.showMessageDialog(JPasswordField.this,
+           "Success!");
+        } else {
+          JOptionPane.showMessageDialog(JPasswordField.this,
+           "Invalid password", "Error Message",
+            JOptionPane.ERROR_MESSAGE);
+        }
+      }
+    });
     Container cp = getContentPane();
     cp.setLayout(new FlowLayout());
     cp.add(passField);
