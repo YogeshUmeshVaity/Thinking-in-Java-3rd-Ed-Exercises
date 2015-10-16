21cbf0177a0e61d1568a9ddb44cc17521f665183
Yogesh <yogeshvaity@gmail.com>
2015-10-16 11:35:36 +0530

    finish 14ex6

--- ch14/ex6/AllComponents.java
+++ ch14/ex6/AllComponents.java
@@ -4,6 +4,7 @@
  */
 
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.plaf.basic.*;
@@ -29,9 +30,27 @@ public class AllComponents extends JApplet {
     new JRadioButton("JRadioButton2");
   private JTextField text = new JTextField(21);
   private JTextArea textArea = new JTextArea(6, 21);
-  private JScrollPane scrollPane = new JScrollPane(textArea,
+  private JScrollPane scrollPaneTextArea = new JScrollPane(textArea,
     JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
     JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+  private JTextPane textPane = new JTextPane();
+  private JScrollPane scrollPaneTextPane = new JScrollPane(
+    textPane);
+  private JComboBox comboBox = new JComboBox();
+  private String[] comboList = {
+    "JellyBean", "KitKat", "Lollipop"
+  };
+  private DefaultListModel itemList = new DefaultListModel();
+  private JList list = new JList(itemList);
+  private JTabbedPane tabs = new JTabbedPane();
+  private String[] dialogs = {
+    "Message Dialog", "Confirm Dialog"
+  };
+  private JMenuBar menuBar = new JMenuBar();
+  private JMenu file = new JMenu("File");
+  private JMenuItem save = new JMenuItem("Save");
+  private JPopupMenu popup = new JPopupMenu();
+  private JMenuItem popupMenuItem = new JMenuItem("Copy");
   
   @Override
   public void init() {
@@ -58,9 +77,70 @@ public class AllComponents extends JApplet {
     jp.add(right);
     jp.add(left);
     cp.add(jp);
-    
+    // Text
     cp.add(text);
-    cp.add(scrollPane);
+    cp.add(scrollPaneTextArea);
+    textPane.setText("This is a JTextPane");
+    cp.add(scrollPaneTextPane);
+    // ComboBox
+    for(int i = 0; i < comboList.length; i++) {
+      comboBox.addItem(comboList[i]);
+    }
+    cp.add(comboBox);
+    // JList
+    for(int i = 0; i < comboList.length; i++) {
+      itemList.add(0, comboList[i]);
+    }
+    cp.add(list);
+    // JTabbedPane
+    for(int i = 0; i < dialogs.length; i++) {
+      tabs.addTab(dialogs[i], new JButton("Show " +
+        dialogs[i]));
+    }
+    tabs.addChangeListener(new ChangeListener() {
+    
+      @Override
+      public void stateChanged(ChangeEvent e) {
+        if(checkBox.isSelected()) {
+          JOptionPane.showMessageDialog(null, "Message Dialog " +
+            "test", "AllComponents", JOptionPane.ERROR_MESSAGE);
+        } else {
+          JOptionPane.showConfirmDialog(null, "Confirm Dialog" +
+            " test", "Warning", JOptionPane.DEFAULT_OPTION
+            );
+        }
+        
+      }
+      
+    });
+    cp.add(tabs);
+    // Menu
+    file.add(save);
+    menuBar.add(file);
+    setJMenuBar(menuBar);
+    // Popup Menu
+    popup.add(popupMenuItem);
+    cp.add(popup);
+    addMouseListener(new MouseAdapter() {
+    
+      @Override
+      public void mousePressed(MouseEvent e) {
+        maybeShowPopup(e);
+      }
+      
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        maybeShowPopup(e);
+      }
+      
+      private void maybeShowPopup(MouseEvent e) {
+        if(e.isPopupTrigger()) {
+          popup.show(((JApplet)e.getComponent())
+          .getContentPane(), e.getX(), e.getY());
+        }
+      }
+      
+    });
   }
   
   public static void main(String[] args) {
